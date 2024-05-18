8a247e07a13f3c7bb60401e76575fa2f23eeee3f
tungb12ok <99113840+tungb12ok@users.noreply.github.com>
2024-05-18 22:24:20 +0700

    Update Change

--- summer2024-swp391.se1811-g1-20240518T151326Z-001/summer2024-swp391.se1811-g1/OnlineShopping/src/java/dao/UserDAO.java
+++ summer2024-swp391.se1811-g1-20240518T151326Z-001/summer2024-swp391.se1811-g1/OnlineShopping/src/java/dao/UserDAO.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package dal;
+package dao;
 
 /**
  *
- * @author ADMINZ
+ * @author tungl
  */
-public class BlogDAO extends DBContext{
+public class UserDAO {
     
 }
